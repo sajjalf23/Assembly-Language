0e993003eaefa24fe7e134a02b5ed994e3cee7dc
Finch <168956586+sajjalf23@users.noreply.github.com>
2024-12-16 17:55:25 +0500

    Add files via upload

---
+++ practice16_printingbox_.asm
@@ -0,0 +1,45 @@
+.model small        ; model directive
+
+.stack 100h         ; stack segment directive
+
+.data               ; data segment directive
+    
+    ; An AL Program to display a 10*10 solid box of asterisks
+    string1 db '********** $'
+
+.code            ; code segment directive
+   
+     main proc
+
+    mov ax , @data    ; load address of data segment directive
+    mov ds , ax       ; load address of data segment directive to data segment register
+
+    mov dx , offset string1
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+    mov ah , 9
+    int 21h
+
+    mov ah , 4ch
+    int 21h
+
+
+    
+    end main
+main end
